0257fb6a88e8ff3ae3cdfeb9d86f96f7c1be1007
josetpm <perezm.josetomas@gmail.com>
2026-04-07 16:36:39 +0200

    fix useAuth import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./context/useAuth";
 import PrivateRoute from "./components/PrivateRoute";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
